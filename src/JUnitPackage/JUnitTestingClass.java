86605b2db3a7f3e2aa5d280b0bc475490dd77a3e
91995 <91995@10.12.16.172>
2022-11-18 10:21:24 +0530

    This is the final project

--- src/JUnitPackage/JUnitTestingClass.java
+++ src/JUnitPackage/JUnitTestingClass.java
@@ -1,5 +1,12 @@
 package JUnitPackage;
 
 public class JUnitTestingClass {
+	public int sum(int i, int j) {
+		return i+j;
+	}
+	public String addString(String i,String j) {
+		return i+j;
+	}
 
+	
 }
